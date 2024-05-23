57aa9312c7c6c0aa0599121864d4cd7f09e31499
Hung Chun Fai <arkon.digital.hcf@gmail.com>
2024-05-23 14:10:10 +0800

    artistic refactoring

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -9,6 +9,7 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 uniform sampler2D u_Texture;
+uniform float uOpacity;
 varying vec3 color;
 varying vec2 vUv;
 varying float radius;
@@ -17,5 +18,5 @@ void main() {
     vec4 ttt = texture2D(u_Texture, vUv);
     // brighter at center, max radius should follow what's defined in index.js
     float oF = smoothstep(0., 0.7, 2.8-radius);
-    gl_FragColor = vec4(color, ttt.r*oF);
+    gl_FragColor = vec4(color, ttt.a * uOpacity);
 }
